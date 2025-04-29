10a26eafebaf87f0a542e91560d0a09107fb3fb3
Bruno Martins <bgcngm@gmail.com>
2025-04-30 00:24:23 +0100

    cas: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,9 +12,6 @@ DEVICE_PATH := device/xiaomi/cas
 # Display
 TARGET_SCREEN_DENSITY := 440
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_cas
-
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/xiaomi/cas.config
 
--- device.mk
+++ device.mk
@@ -30,6 +30,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,qdcm_calib_data_*.xml,$(LOCAL_PATH)/qdcm/,$(TARGET_COPY_OUT_VENDOR)/etc/)
 
+# Init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_cas)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
